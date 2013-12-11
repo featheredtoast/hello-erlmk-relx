e4cf6a31a000d2740e209cfd56d6625d5fa46723
Jeff Wong <awole20@gmail.com>
2013-12-10 23:59:33 -0800

    adding basic logging to files

--- src/foo_app.erl
+++ src/foo_app.erl
@@ -2,6 +2,10 @@
 -behaviour(application).
 -export([start/2, stop/1]).
 start(_StartType, _StartArgs) ->
+    Priv = code:priv_dir(foo),
+    io:format("loading" ++ Priv ++ "~n",[]),
+    {ok, Config} = file:consult(Priv ++ "/app.config"),
+    [application:set_env(foo,K,V)||{K,V}<-Config],
     foo_sup:start_link().
 stop(_State) ->
     ok.
